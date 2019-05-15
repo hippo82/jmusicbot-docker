0c4edcb63506d546adcb7f0edb98529640331871
hippo82 <hipa82@gmail.com>
2019-05-15 22:29:03 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
- if [ ! -e /opt/bot ]
-  then
-    mkdir /opt/bot
- fi
- if [ ! -e /opt/bot/Playlists ]
-  then
-	mkdir /opt/bot/Playlists 
- fi
- if [ ! -e /opt/bot/config.txt ]
+if [ ! -e /opt/bot ]
  then
-      wget --directory-prefix=/opt/bot/ https://github.com/jagrosh/MusicBot/releases/download/VERSION/config.txt
- fi
- if [ ! -e /opt/bot/JMusicBot-VERSION-Linux.jar ]
- then
-          wget --directory-prefix=/opt/bot/ https://github.com/jagrosh/MusicBot/releases/download/VERSION/JMusicBot-VERSION-Linux.jar
+  mkdir /opt/bot
+ elif [ ! -e /opt/bot/Playlists ]
+  then
+   mkdir /opt/bot/Playlists 
+ elif [ ! -e /opt/bot/config.txt ]
+  then
+   echo "Copy config..."
+   cp -p /opt/tmp/config.txt /opt/bot
+ elif [ ! -e /opt/bot/JMusicBot-VERSION-Linux.jar ]
+  then
+   echo "Copy bot..."
+    cp -p /opt/tmp/JMusicBot-VERSION-Linux.jar /opt/bot
+ else
+  echo "all ok"
  fi
 cd /opt/bot/
 java -Dnogui=true -jar JMusicBot-VERSION-Linux.jar
